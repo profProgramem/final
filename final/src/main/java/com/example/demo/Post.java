e8db22ab48f30ec1a4301e48ce14bdbda5897dbd
marcusportela <marcustalegal.mp@gmail.com>
2020-08-07 11:23:13 -0300

    Colocando class post

---
+++ final/src/main/java/com/example/demo/Post.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+public class Post {
+    private String mensagem;
+
+    public void setMensagem(String mensagem){
+        this.mensagem = mensagem;
+    }
+    
+ public String getMensagem(){
+        return this.mensagem;
+    }
+
+}
